1f0a39f7921ce9201dcb119e73cdcfa87cbd2bec
Zhong Qin <17481452@qq.com>
2018-10-09 15:53:51 +0800

    学院控制是否显示论文审核起止时间 (#3)

--- tm-dual-api/grails-app/controllers/cn/edu/bnuz/bell/dual/AwardPublicController.groovy
+++ tm-dual-api/grails-app/controllers/cn/edu/bnuz/bell/dual/AwardPublicController.groovy
@@ -1,24 +1,12 @@
 package cn.edu.bnuz.bell.dual
 
-import cn.edu.bnuz.bell.security.SecurityService
 import org.springframework.security.access.prepost.PreAuthorize
 
 @PreAuthorize('hasRole("ROLE_DUALDEGREE_STUDENT")')
 class AwardPublicController {
-    AwardService awardService
-    SecurityService securityService
-
-    def index() {
-        renderJson(awardService.list(securityService.departmentId))
-    }
+    AwardPublicService awardPublicService
 
     def show( Long id) {
-        def form = awardService.getFormForShow(id)
-        if (form.departmentId != securityService.departmentId) {
-            renderBadRequest()
-        } else {
-           renderJson(form)
-        }
-
+        renderJson awardPublicService.getAwardInfo(id)
     }
 }
